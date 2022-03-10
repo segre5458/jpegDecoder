0b3f62a8405218cd81df045788d4c9bf7eafbc01
segre <segrehouck5458@gmail.com>
2022-03-11 02:26:56 +0900

    sof0セグメント追加

--- parse.go
+++ parse.go
@@ -65,14 +65,35 @@ func Parse(buffer []byte) (err error) {
 			tableNum := make([]int, 0)
 			table := make([][]byte, 0)
 			// ベースライン方式にのみ対応
-			for i := 0; i < (length - 2)/65; i++{
+			for i := 0; i < (length-2)/65; i++ {
 				first, second := Read4BitsAsInt(dqtNR)
 				tablePrecision = append(tablePrecision, first)
 				tableNum = append(tableNum, second)
-				table = append(table, ReadBytes(dqtNR,64))
-				fmt.Println("TableNum:",tableNum[i],"Precision:",tablePrecision[i],"table:",table[i])
+				table = append(table, ReadBytes(dqtNR, 64))
+				fmt.Println("TableNum:", tableNum[i], "Precision:", tablePrecision[i], "table:", table[i])
 			}
 
+		// SOF0セグメント
+		case 0xffc0:
+			sofNR := bytes.NewReader(data)
+			fmt.Println("Segment : SOF")
+			elementPrecision := ReadBytesAsInt(sofNR, 1)
+			heightSize := ReadBytesAsInt(sofNR, 2)
+			widthSize := ReadBytesAsInt(sofNR, 2)
+			elementNum := ReadBytesAsInt(sofNR, 1)
+			fmt.Println("Element Precision:",elementPrecision,"Image Size Height:",heightSize,"Width:",widthSize)
+			cn := make([]int,0)
+			hn := make([]int,0)
+			vn := make([]int,0)
+			tqn := make([]int,0)
+			for i := 0; i < elementNum; i++ {
+				cn = append(cn, ReadBytesAsInt(sofNR,1))
+				first,second := Read4BitsAsInt(sofNR)
+				hn = append(hn, first)
+				vn = append(vn, second)
+				tqn = append(tqn, ReadBytesAsInt(sofNR,1))
+				fmt.Println("Element No.",i,"ID:",cn[i],"Horizontal Level;",hn[i],"Vertical Level:",vn[i],"Table Number:",tqn[i])
+			}
 
 		// SOSセグメント
 		case 0xffda:
